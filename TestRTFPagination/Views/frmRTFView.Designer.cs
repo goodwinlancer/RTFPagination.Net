73fdb89eb750d8c0172c1be185aa2e7eff9f04bd
goodwinlancer <goodwinlancer@gmail.com>
2020-07-09 21:41:49 +0530

    initial version

---
+++ TestRTFPagination/Views/frmRTFView.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace TestRTFPagination.Views
+{
+    partial class frmRTFView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTxtRTF = new System.Windows.Forms.RichTextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.pnlNavigator = new System.Windows.Forms.Panel();
+            this.btnNextPage = new System.Windows.Forms.Button();
+            this.btnPrevPage = new System.Windows.Forms.Button();
+            this.lblPageIndex = new System.Windows.Forms.Label();
+            this.dlgOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.pnlNavigator.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTxtRTF
+            // 
+            this.richTxtRTF.Location = new System.Drawing.Point(1, 0);
+            this.richTxtRTF.Name = "richTxtRTF";
+            this.richTxtRTF.Size = new System.Drawing.Size(796, 430);
+            this.richTxtRTF.TabIndex = 0;
+            this.richTxtRTF.Text = "";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(672, 436);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(122, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load RTF Document";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // pnlNavigator
+            // 
+            this.pnlNavigator.Controls.Add(this.lblPageIndex);
+            this.pnlNavigator.Controls.Add(this.btnPrevPage);
+            this.pnlNavigator.Controls.Add(this.btnNextPage);
+            this.pnlNavigator.Location = new System.Drawing.Point(202, 433);
+            this.pnlNavigator.Name = "pnlNavigator";
+            this.pnlNavigator.Size = new System.Drawing.Size(390, 31);
+            this.pnlNavigator.TabIndex = 2;
+            this.pnlNavigator.Visible = false;
+            // 
+            // btnNextPage
+            // 
+            this.btnNextPage.Location = new System.Drawing.Point(278, 4);
+            this.btnNextPage.Name = "btnNextPage";
+            this.btnNextPage.Size = new System.Drawing.Size(108, 23);
+            this.btnNextPage.TabIndex = 2;
+            this.btnNextPage.Text = "Next Page >>";
+            this.btnNextPage.UseVisualStyleBackColor = true;
+            this.btnNextPage.Click += new System.EventHandler(this.btnNextPage_Click);
+            // 
+            // btnPrevPage
+            // 
+            this.btnPrevPage.Location = new System.Drawing.Point(4, 4);
+            this.btnPrevPage.Name = "btnPrevPage";
+            this.btnPrevPage.Size = new System.Drawing.Size(108, 23);
+            this.btnPrevPage.TabIndex = 1;
+            this.btnPrevPage.Text = "<< Previous Page";
+            this.btnPrevPage.UseVisualStyleBackColor = true;
+            this.btnPrevPage.Click += new System.EventHandler(this.btnPrevPage_Click);
+            // 
+            // lblPageIndex
+            // 
+            this.lblPageIndex.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPageIndex.Location = new System.Drawing.Point(118, 4);
+            this.lblPageIndex.Name = "lblPageIndex";
+            this.lblPageIndex.Size = new System.Drawing.Size(154, 23);
+            this.lblPageIndex.TabIndex = 2;
+            this.lblPageIndex.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dlgOpenFile
+            // 
+            this.dlgOpenFile.FileName = "openFileDialog1";
+            // 
+            // frmRTFView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 468);
+            this.Controls.Add(this.pnlNavigator);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.richTxtRTF);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmRTFView";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "View RTF Document";
+            this.pnlNavigator.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTxtRTF;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Panel pnlNavigator;
+        private System.Windows.Forms.Label lblPageIndex;
+        private System.Windows.Forms.Button btnPrevPage;
+        private System.Windows.Forms.Button btnNextPage;
+        private System.Windows.Forms.OpenFileDialog dlgOpenFile;
+    }
+}
---
+++ TestRTFPagination/Views/frmRTFView.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestRTFPagination.Views
+{
+    public partial class frmRTFView : Form
+    {
+        //Variable to store pages of the RTF document
+        private String[] pages;
+
+        //Variable to store the document header
+        private String[] documentHeader = new String[2];
+
+        //Variable to store the currentPageIndex
+        private int currentPageIndex = 0;
+
+        public frmRTFView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            //Select RTF file 
+            String fileName = null;
+            try
+            {
+                fileName = SelectFileToOpen();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("No document was selected", "Select RTF file to view",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+
+            //Try to load the selected RTF document
+            try
+            {
+                //Load document
+                LoadDocument(fileName);
+                //Load document header
+                LoadDocumentHeader();
+                //Show the content in the control
+                ShowPageContent();
+                //Modify navigation panel
+                ShowNavigationPanel();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Could not parse the document. Reason: " + ex.Message, "Parse error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+        }
+
+        private void LoadDocument(String fileName)
+        {
+            //Best way to load the content of an RTF document is using the Rich Textbox Control
+
+            //Load the file into Rich Textbox Control
+            richTxtRTF.LoadFile(fileName);
+            this.Text = "Viewing file: " + fileName;
+
+            //Get the String content from the Rich Textbox Control
+            String content = richTxtRTF.Rtf;
+
+            //Split the content on the basis of pagebreak markers
+            //And prepare the pages array
+            //Each element of the array will be a separate page of the document
+            String[] separatingStrings = { @"\page\par" };
+            this.pages = content.Split(separatingStrings, System.StringSplitOptions.None);
+        }
+        private void LoadDocumentHeader()
+        {
+            String[] separatingStrings = { "\r\n" };
+
+            String[] lines = pages[0].Split(separatingStrings, System.StringSplitOptions.None);
+
+            //First two lines contain document header
+            int lineNumber = 0;
+            foreach (String line in lines)
+            {
+                this.documentHeader[lineNumber] = line;
+                lineNumber++;
+                if (lineNumber > 1)
+                {
+                    break;
+                }
+            }
+        }
+
+        private String SelectFileToOpen()
+        {
+            dlgOpenFile = new OpenFileDialog
+            {
+
+                InitialDirectory = Directory.GetCurrentDirectory(),
+
+                Title = "Select RTF file to view",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "rtf",
+                Filter = "RTF (*.rtf)|*.rtf|All (*.*)|*.*",
+                FilterIndex = 0,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = false,
+                ShowReadOnly = false
+            };
+
+            if (dlgOpenFile.ShowDialog() == DialogResult.OK)
+            {
+                return dlgOpenFile.FileName;
+            }
+            else
+            {
+                throw new Exception("No files selected");
+            }
+        }
+
+        private void ShowPageContent()
+        {
+            if (this.currentPageIndex == 0)
+            {
+                //Do not add header, because header is already available in the first page
+                richTxtRTF.Rtf = pages[this.currentPageIndex];
+            }
+            else
+            {
+                //Add the document header
+                richTxtRTF.Rtf = this.documentHeader[0] + this.documentHeader[1] + this.pages[this.currentPageIndex];
+            }
+        }
+
+        private void ShowNavigationPanel()
+        {
+            pnlNavigator.Visible = true;
+            if(this.currentPageIndex == 0) //First page
+            {
+                btnPrevPage.Enabled = false;
+            }
+
+            if(this.currentPageIndex == (this.pages.Length - 1)) //Last page
+            {
+                btnNextPage.Enabled = false;
+            }
+
+            if (this.currentPageIndex < (this.pages.Length - 1)) //Not the last page
+            {
+                btnNextPage.Enabled = true;
+            }
+
+            if (this.currentPageIndex > 0) //Not the first page
+            {
+                btnPrevPage.Enabled = true;
+            }
+
+            lblPageIndex.Text = "Current page #" + (this.currentPageIndex + 1) + " of " + this.pages.Length;
+        }
+
+        private void btnNextPage_Click(object sender, EventArgs e)
+        {
+            this.currentPageIndex++;
+            ShowPageContent();
+            ShowNavigationPanel();
+        }
+
+        private void btnPrevPage_Click(object sender, EventArgs e)
+        {
+            this.currentPageIndex--;
+            ShowPageContent();
+            ShowNavigationPanel();
+
+        }
+    }
+}
